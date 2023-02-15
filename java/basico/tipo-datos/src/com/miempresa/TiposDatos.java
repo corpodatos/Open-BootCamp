78623947ad44a5a9c1ec21c4105704a72ee697cf
corpodatos <edwinrivero@yahoo.com>
2023-02-15 12:53:42 -0300

    creación de ejercicio tipos de datos

---
+++ java/basico/tipo-datos/src/com/miempresa/TiposDatos.java
@@ -0,0 +1,39 @@
+package com.miempresa;
+
+/**
+ * Ejercicio1 con tipode de datos java
+ *
+ * */
+
+public class TiposDatos {
+    public static void main(String[] args) {
+        System.out.println("Tipos de datos en java:");
+
+        //Numéricos
+        byte tipoByte = 8;
+        System.out.println("tipoByte -> " + tipoByte);
+        short tipoShort = 25;
+        System.out.println("tipoShort -> " + tipoShort);
+        int tipoEntero = 1976;
+        System.out.println("tipoEntero -> " + tipoEntero);
+        long tipoLong = 10000;
+        System.out.println("tipoLong -> " + tipoLong);
+        float tipoFloat = 5.5f;
+        System.out.println("tipoFloat -> " + tipoFloat);
+        double tipoDouble = 4535.45d;
+        System.out.println("tipoDouble -> " + tipoDouble);
+
+        //Booleanos
+        boolean tipoBooleanoVerdadero = true;
+        System.out.println("tipoBooleanoVerdadero -> " + tipoBooleanoVerdadero);
+        boolean tipoBooleanFalso = false;
+        System.out.println("tipoBooleanFalso -> " + tipoBooleanFalso);
+
+        //Texto
+        char tipoChar = 'E';
+        System.out.println("tipoChar -> " + tipoChar);
+        String tipoString = "Cadena de texto";
+        System.out.println("tipoString -> " + tipoString);
+
+    }
+}
